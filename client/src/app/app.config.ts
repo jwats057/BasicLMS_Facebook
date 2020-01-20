9e4cae43f37ff4b59a340861f51a62eff5155bee
pantharev <narev001@fiu.edu>
2020-01-19 20:52:40 -0500

    Added config file

---
+++ client/src/app/app.config.ts
@@ -0,0 +1,7 @@
+// Please replace/update values
+
+export const FacebookID = "474980013435668"; // Facebook App ID
+export const FB_API_VER = 'v5.0'; // Facebook API Version
+export const SUPPORT = [
+    {name: 'Nicolas Arevalo', phone: '(555)-555-5555', email: 'mail@mail.com'}
+];
